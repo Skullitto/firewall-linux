d78dc58cb79e8b6f2876c827cc6f3c5e97b3ca1c
Skull-Sky <skull-sky@protonmail.com>
2020-03-22 19:24:47 +0000

    Script de remise à zéro des régles

---
+++ flush.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Remise à zéro..."
+sleep 1
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+echo "Terminé ..."
